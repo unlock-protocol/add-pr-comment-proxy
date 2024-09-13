25c74f30bad37c821be7ae34a8dfd320d5dd73bc
Julien Genestoux <julien.genestoux@gmail.com>
2024-09-13 11:14:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 
 app.get('/test', async (req, res) => {
-  res.status(200).send('hello')
+  res.status(200).send('hello world')
 })
 
 app.post('/repos/:owner/:repo/issues/:issueNumber/comments', async (req, res, next) => {
